6ef93e453302ed68ad00a0cb231dd18febd7c989
Nate Catelli <ncatelli@packetfire.org>
2020-01-26 19:05:02 -0600

    First pass at stubbed webserver.

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/ncatelli/mockserver
+
+require github.com/gorilla/mux v1.7.3
+
+go 1.13
--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+const (
+	addr string = "0.0.0.0:8080"
+)
 
 func main() {
-	fmt.Print("Hello world!")
+	mux := mux.NewRouter()
+	mux.HandleFunc(`/healthcheck`, healthHandler).Methods("GET")
+
+	log.Printf("Starting server on %s\n", addr)
+	if err := http.ListenAndServe("addr", mux); err != nil {
+		log.Fatalf("Error in ListenAndServe: %s", err)
+	}
 }
