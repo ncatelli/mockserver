59fd1a9c83da5e5cc2ce5783870054b3bfb13170
Nate Catelli <ncatelli@packetfire.org>
2020-01-29 20:17:45 -0600

    Stub ServeHTTP in route and handler.

--- pkg/router/handler.go
+++ pkg/router/handler.go
@@ -1,5 +1,10 @@
 package router
 
+import (
+	"fmt"
+	"net/http"
+)
+
 // Handler includes all the metadata to decide on and serve a response.
 type Handler struct {
 	Weight          int               `yaml:"weight"`
@@ -7,4 +12,9 @@ type Handler struct {
 	StaticResponse  string            `yaml:"static_response"`
 	ResponseStatus  int               `yaml:"response_status"`
 	ResponsePath    string            `yaml:"response_path"`
+}
+
+// ServeHTTP implements the http.Handler interface eventually serving a request.
+func (handler *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Ok") // STUB
 }
--- pkg/router/route.go
+++ pkg/router/route.go
@@ -1,5 +1,7 @@
 package router
 
+import "net/http"
+
 // Route includes all routing data to build a route and forward to an
 // appropriate router. This is handed off to the router for the live routing.
 type Route struct {
@@ -8,4 +10,11 @@ type Route struct {
 	QueryParams    map[string]string `yaml:"query_params"`
 	RequestHeaders map[string]string `yaml:"request_headers"`
 	Handlers       []Handler         `yaml:"handlers"`
+	totalWeight    uint
+}
+
+// ServeHTTP implements the http.Handler interface for pipelining a request
+// further into a handler.
+func (route *Route) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
 }
