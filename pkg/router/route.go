27389136ccf67f80fc647a643f9d16c2d3e5cbff
Nate Catelli <ncatelli@packetfire.org>
2020-02-02 18:44:34 -0600

    Add weighted routing.

--- pkg/router/route.go
+++ pkg/router/route.go
@@ -1,6 +1,15 @@
 package router
 
-import "net/http"
+import (
+	"fmt"
+	"math/rand"
+	"net/http"
+	"time"
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
 
 // Route includes all routing data to build a route and forward to an
 // appropriate router. This is handed off to the router for the live routing.
@@ -10,11 +19,38 @@ type Route struct {
 	QueryParams    map[string]string `yaml:"query_params"`
 	RequestHeaders map[string]string `yaml:"request_headers"`
 	Handlers       []Handler         `yaml:"handlers"`
-	totalWeight    uint
+	totalWeight    int
 }
 
 // ServeHTTP implements the http.Handler interface for pipelining a request
 // further into a handler.
 func (route *Route) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if route.totalWeight == 0 {
+		route.totalWeight = route.calculateTotalWeightofHandlers()
+	}
 
+	hw := rand.Intn(route.totalWeight + 1)
+
+	for _, h := range route.Handlers {
+		hw -= h.Weight
+		if hw <= 0 {
+			h.ServeHTTP(w, r)
+			return
+		}
+	}
+
+	w.WriteHeader(http.StatusInternalServerError)
+	fmt.Fprint(w, "unable to determine routable handler")
+}
+
+// calculateTotalWeightofHandlers iterates over all handlers assigned to the
+// route and sums their total weight.
+func (route *Route) calculateTotalWeightofHandlers() int {
+	var weight int
+
+	for _, h := range route.Handlers {
+		weight += h.Weight
+	}
+
+	return weight
 }
