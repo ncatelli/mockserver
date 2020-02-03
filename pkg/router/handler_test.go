f887cd0b0806e43d62d7a2d17bbd1f5974d25712
Nate Catelli <ncatelli@packetfire.org>
2020-02-02 19:43:25 -0600

    Adding basic functionality tests.

--- pkg/router/handler_test.go
+++ pkg/router/handler_test.go
@@ -1,14 +1,17 @@
 package router
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"reflect"
 	"testing"
 
+	"github.com/gorilla/mux"
 	"gopkg.in/yaml.v2"
 )
 
-func TestHandlerUnmarshaling(t *testing.T) {
-	t.Run("should unmarshal to the correct keys", func(t *testing.T) {
+func TestHandlerUnmarshalingShould(t *testing.T) {
+	t.Run("unmarshal to the correct keys", func(t *testing.T) {
 		expectedHandler := Handler{
 			Weight: 1,
 		}
@@ -22,6 +25,70 @@ func TestHandlerUnmarshaling(t *testing.T) {
 
 		if !reflect.DeepEqual(expectedHandler, handler) {
 			t.Errorf(errFmt, expectedHandler, handler)
+		}
+	})
+}
+
+func TestHandlerServeHTTPMethodShould(t *testing.T) {
+	t.Run("serve a static response when set", func(t *testing.T) {
+		h := &Handler{
+			StaticResponse: "Ok",
+			ResponseStatus: 200,
+		}
+
+		req, err := http.NewRequest("GET", "/", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		router := mux.NewRouter()
+		router.Handle("/", h).Methods("GET")
+
+		rr := httptest.NewRecorder()
+		router.ServeHTTP(rr, req)
+
+		// Check the status code is what we expect.
+		if status := rr.Code; status != http.StatusOK {
+			t.Errorf("handler returned wrong status code: got %v want %v",
+				status, http.StatusOK)
+		}
+
+		// Check the response body is what we expect.
+		expected := `Ok`
+		if rr.Body.String() != expected {
+			t.Errorf("handler returned unexpected body: got %v want %v",
+				rr.Body.String(), expected)
+		}
+	})
+
+	t.Run("serve a template from a file when specified", func(t *testing.T) {
+		h := &Handler{
+			ResponseStatus: 200,
+			ResponsePath:   "test_fixtures/good_simple_string.txt",
+		}
+
+		req, err := http.NewRequest("GET", "/", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		router := mux.NewRouter()
+		router.Handle("/", h).Methods("GET")
+
+		rr := httptest.NewRecorder()
+		router.ServeHTTP(rr, req)
+
+		// Check the status code is what we expect.
+		if status := rr.Code; status != http.StatusOK {
+			t.Errorf("handler returned wrong status code: got %v want %v",
+				status, http.StatusOK)
+		}
+
+		// Check the response body is what we expect.
+		expected := `Ok`
+		if rr.Body.String() != expected {
+			t.Errorf("handler returned unexpected body: got %v want %v",
+				rr.Body.String(), expected)
 		}
 	})
 }
