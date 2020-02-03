86876f0436f291786ea087897ab53d3e3b95ad59
Nate Catelli <ncatelli@packetfire.org>
2020-02-02 19:01:51 -0600

    Add Handler logic.

--- pkg/router/handler.go
+++ pkg/router/handler.go
@@ -1,7 +1,8 @@
 package router
 
 import (
-	"fmt"
+	"html/template"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -12,9 +13,53 @@ type Handler struct {
 	StaticResponse  string            `yaml:"static_response"`
 	ResponseStatus  int               `yaml:"response_status"`
 	ResponsePath    string            `yaml:"response_path"`
+	bodyTemplate    *template.Template
+}
+
+// getBodyTemplate will attempt to retrieve, preferably from a cache field, the
+// template used to generate the response body of a Handler.
+func (handler *Handler) getBodyTemplate() (*template.Template, error) {
+	// short circut if the template is cached
+	if handler.bodyTemplate != nil {
+		return handler.bodyTemplate, nil
+	}
+
+	// placeholder for future template data.
+	var body string
+
+	// static response is highest precedence
+	if len(handler.StaticResponse) > 0 {
+		body = handler.StaticResponse
+	} else if len(handler.ResponsePath) > 0 {
+		bb, err := ioutil.ReadFile(handler.ResponsePath)
+		if err != nil {
+			return nil, err
+		}
+
+		body = string(bb)
+	}
+
+	t, err := template.New("").Parse(body)
+	if err != nil {
+		return nil, err
+	}
+
+	handler.bodyTemplate = t
+	return t, nil
 }
 
 // ServeHTTP implements the http.Handler interface eventually serving a request.
 func (handler *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Ok") // STUB
+	t, err := handler.getBodyTemplate()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	for h, v := range handler.ResponseHeaders {
+		w.Header().Set(h, v)
+	}
+
+	w.WriteHeader(handler.ResponseStatus)
+	t.Execute(w, nil)
 }
