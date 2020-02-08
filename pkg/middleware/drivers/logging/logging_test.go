75f76ac2eb249e05dac575bd8402a5f350551ff7
Nate Catelli <ncatelli@packetfire.org>
2020-02-08 11:47:27 -0600

    Add additional logging cases for logger.

--- pkg/middleware/drivers/logging/logging_test.go
+++ pkg/middleware/drivers/logging/logging_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
 
 	"github.com/gorilla/mux"
@@ -24,17 +25,47 @@ func TestLoggingMiddlewareShould(t *testing.T) {
 		router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {}).Methods("GET")
 
 		logBuffer := new(bytes.Buffer)
-		logMiddleWare := &Middleware{}
-		logMiddleWare.Init(map[string]string{})
-		logMiddleWare.target = logBuffer
+		logMiddleware := &Middleware{}
+		logMiddleware.target = logBuffer
 
-		router.Use(logMiddleWare.Middleware)
+		router.Use(logMiddleware.Middleware)
 
 		rr := httptest.NewRecorder()
 		router.ServeHTTP(rr, req)
 
 		if !bytes.Contains(logBuffer.Bytes(), []byte("GET /")) {
 			t.Errorf(errFmt, "GET /", logBuffer.Bytes())
+		}
+	})
+
+	t.Run("logger target should default to os.Stdout if no target is passed to Init", func(t *testing.T) {
+		logMiddleware := &Middleware{}
+		logMiddleware.Init(map[string]string{})
+
+		if logMiddleware.target != os.Stdout {
+			t.Errorf(errFmt, os.Stdout, logMiddleware.target)
+		}
+	})
+
+	t.Run("logger target should be set to os.Stdout if stdout is passed in Init", func(t *testing.T) {
+		logMiddleware := &Middleware{}
+		logMiddleware.Init(map[string]string{
+			"target": "stdout",
+		})
+
+		if logMiddleware.target != os.Stdout {
+			t.Errorf(errFmt, os.Stdout, logMiddleware.target)
+		}
+	})
+
+	t.Run("throw an error if an unknown target is specified", func(t *testing.T) {
+		logMiddleware := &Middleware{}
+		err := logMiddleware.Init(map[string]string{
+			"target": "this target doesn't exist",
+		})
+
+		if err == nil {
+			t.Errorf(errFmt, "error", nil)
 		}
 	})
 }
