2cc5843ec047a1fdeeda3eff037a17ee8b71db7f
Nate Catelli <ncatelli@packetfire.org>
2020-02-09 09:34:33 -0600

    Make test parallel.

--- pkg/router/middleware/middlewares/latency/latency_test.go
+++ pkg/router/middleware/middlewares/latency/latency_test.go
@@ -15,36 +15,38 @@ const (
 )
 
 func TestLatencyMiddlewareShould(t *testing.T) {
-	var wg sync.WaitGroup
-	for i := 0; i < 1000; i++ {
-		wg.Add(1)
+	t.Run("stay within a range of responses.", func(t *testing.T) {
+		var wg sync.WaitGroup
+		for i := 0; i < 1000; i++ {
+			wg.Add(1)
 
-		go func() {
-			defer wg.Done()
-			req, err := http.NewRequest("GET", "/", nil)
-			if err != nil {
-				t.Fatal(err)
-			}
+			go func() {
+				defer wg.Done()
+				req, err := http.NewRequest("GET", "/", nil)
+				if err != nil {
+					t.Fatal(err)
+				}
 
-			router := mux.NewRouter()
-			router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {}).Methods("GET")
+				router := mux.NewRouter()
+				router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {}).Methods("GET")
 
-			latencyMiddleware := &Middleware{Latency: 1000}
+				latencyMiddleware := &Middleware{Latency: 1000}
 
-			router.Use(latencyMiddleware.Middleware)
+				router.Use(latencyMiddleware.Middleware)
 
-			rr := httptest.NewRecorder()
+				rr := httptest.NewRecorder()
 
-			// start timer
-			start := time.Now()
-			router.ServeHTTP(rr, req)
-			duration := time.Since(start)
+				// start timer
+				start := time.Now()
+				router.ServeHTTP(rr, req)
+				duration := time.Since(start)
 
-			if duration.Milliseconds() < 1000 || duration.Milliseconds() > 2000 {
-				t.Errorf(errFmt, "between 1000 - 2000", duration.Milliseconds())
-			}
-		}()
-	}
+				if duration.Milliseconds() < 1000 || duration.Milliseconds() > 2000 {
+					t.Errorf(errFmt, "between 1000 - 2000", duration.Milliseconds())
+				}
+			}()
+		}
 
-	wg.Wait()
+		wg.Wait()
+	})
 }
