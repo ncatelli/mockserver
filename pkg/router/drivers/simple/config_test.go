7170e6474a1c458cd44f20365e91d4f2505bacb4
Nate Catelli <ncatelli@packetfire.org>
2020-02-16 17:52:16 -0600

    Add static data to config_tests.

--- pkg/router/drivers/simple/config_test.go
+++ pkg/router/drivers/simple/config_test.go
@@ -1,8 +1,8 @@
 package simple
 
 import (
+	"bytes"
 	"log"
-	"os"
 	"path/filepath"
 	"reflect"
 	"testing"
@@ -14,6 +14,25 @@ const (
 	goodConfigPath string = "test_fixtures/good.yaml"
 	badConfigPath  string = "test_fixtures/bad.yaml"
 	errFmt         string = "want %v, got %v"
+)
+
+var (
+	goodConfig = []byte(`
+- path: "/test/weighted/errors"
+  method: GET
+  handlers:
+    - weight: 2
+      response_headers:
+        content-type: application/json
+      static_response: '{"resp": "Ok"}'
+      response_status: 200
+    - weight: 1
+      response_headers:
+        content-type: text/plain
+      static_response: ''
+      response_status: 500
+`)
+	badConfig = []byte(";189na--ac")
 )
 
 var expectedRoutes = []*router.Route{
@@ -42,11 +61,7 @@ var expectedRoutes = []*router.Route{
 
 func TestLoadShould(t *testing.T) {
 	t.Run("load a valid configuration", func(t *testing.T) {
-		gp, err := filepath.Rel("", goodConfigPath)
-		file, err := os.Open(gp)
-		defer file.Close()
-
-		routes, err := Load(file)
+		routes, err := Load(bytes.NewReader(goodConfig))
 		if err != nil {
 			t.Errorf(errFmt, expectedRoutes, err)
 		} else if !reflect.DeepEqual(routes, expectedRoutes) {
@@ -54,16 +69,9 @@ func TestLoadShould(t *testing.T) {
 		}
 	})
 
-	t.Run("return an error a non-yaml parseable file.", func(t *testing.T) {
-		bp, err := filepath.Rel("", badConfigPath)
-		file, err := os.Open(bp)
-		defer file.Close()
-
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		if _, err := LoadFromFile(bp); err == nil {
+	t.Run("return an error on a non-valid configuration", func(t *testing.T) {
+		_, err := Load(bytes.NewReader(badConfig))
+		if err == nil {
 			t.Errorf(errFmt, "an error", err)
 		}
 	})
