bb33ec9c50be0dfab9de6f8c22f4ded67154d684
Nate Catelli <ncatelli@packetfire.org>
2020-02-16 11:26:59 -0600

    Stub Load method.

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,6 +1,9 @@
 package config
 
 import (
+	"io"
+	"net/url"
+
 	"github.com/caarlos0/env/v6"
 )
 
@@ -8,8 +11,9 @@ import (
 // global level. This can include listening address, feature flags and other
 // configurations.
 type Config struct {
-	Addr       string `env:"ADDR" envDefault:"0.0.0.0:8080"`
-	ConfigPath string `env:"CONFIG_PATH"`
+	Addr       string  `env:"ADDR" envDefault:"0.0.0.0:8080"`
+	ConfigPath string  `env:"CONFIG_PATH"`
+	ConfigURL  url.URL `env:"CONFIG_URL"`
 }
 
 // New initializes a Config, attempting to parse parames from Envs.
@@ -21,4 +25,11 @@ func New() (Config, error) {
 	}
 
 	return c, nil
+}
+
+// Load attempts to fetch a Router configuration from one of the optional
+// locations (URL or Filepath). On success it returns an io.Reader for this
+// file otherwise an error is returned.
+func (c *Config) Load() (io.Reader, error) {
+	return nil, nil
 }
