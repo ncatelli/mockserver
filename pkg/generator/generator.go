0bd46b252658cdc7bab33a7270eabde68edb72da
Nate Catelli <ncatelli@packetfire.org>
2020-03-08 18:24:53 -0500

    First pass at generator.

--- pkg/generator/generator.go
+++ pkg/generator/generator.go
@@ -1,11 +1,8 @@
 package generator
 
-import "math/rand"
-
 // Generator defines the interface for types to be added to the templating
 // FuncMap and exposed to the handlers.
 type Generator interface {
-	Init(rand.Rand) error
 	ID() string
-	Generate(...string) string
+	Generate(...interface{}) interface{}
 }
