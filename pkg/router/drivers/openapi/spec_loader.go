de460375f6dda10582b2453bbe2633a262afd7a5
Nate Catelli <ncatelli@packetfire.org>
2020-03-22 13:23:35 -0500

    Begin adding methods to parse out spec operations

--- pkg/router/drivers/openapi/spec_loader.go
+++ pkg/router/drivers/openapi/spec_loader.go
@@ -2,15 +2,36 @@ package openapi
 
 import (
 	"encoding/json"
-	"fmt"
 	"io"
 	"io/ioutil"
+	"reflect"
+	"strings"
 
 	"github.com/go-openapi/loads"
+	"github.com/go-openapi/spec"
 	"github.com/ncatelli/mockserver/pkg/router"
 )
 
-// Load takes an io.Reader and attempts to unmarshal the configuration into a
+// opMap represents a kv pairing of specOperations to their corresponding
+// http method.
+type opMap map[string]*spec.Operation
+
+// op stores a method name and swagger.Operation pairing
+type op struct {
+	Name string
+	Op   *spec.Operation
+}
+
+// newOp initializes and returns a pointer to an op after formatting its
+// parameters.
+func newOp(n string, o *spec.Operation) *op {
+	return &op{
+		Name: strings.ToUpper(n),
+		Op:   o,
+	}
+}
+
+// Load takes an io.Reader and attempts to unmarshal the swagger spec into a
 // route slice. On success, a slice of routes and nil is returned, otherwise an
 // error is returned.
 func Load(data io.Reader) ([]*router.Route, error) {
@@ -25,7 +46,51 @@ func Load(data io.Reader) ([]*router.Route, error) {
 		return routes, err
 	}
 
-	fmt.Print(swagger.Spec().Info.Version)
+	ss := swagger.Spec()
+	for path, params := range ss.Paths.Paths {
+		for _, operation := range getPathItemOperations(params) {
+			r, err := generateRouteFromSwaggerPathOperation(path, operation)
+			if err != nil {
+				return routes, err
+			}
+
+			routes = append(
+				routes,
+				r,
+			)
+		}
+	}
 
 	return routes, nil
+}
+
+// Takes swagger spec parameters and attempts to package them into a
+// corresponding mock server route.
+func generateRouteFromSwaggerPathOperation(urlPath string, o *op) (*router.Route, error) {
+	return &router.Route{
+		Path: urlPath,
+	}, nil
+}
+
+// getPathItemOperations takes a PathItem and iterates over all
+// Operations defined on the map, returning any non-nil Operations.
+func getPathItemOperations(p spec.PathItem) []*op {
+	props := p.PathItemProps
+	ops := make([]*op, 0, 1)
+
+	sV := reflect.ValueOf(props)
+	sT := reflect.TypeOf(props)
+
+	for i := 0; i < sV.NumField(); i++ {
+		method := sT.Field(i).Name
+		if method == "Parameters" {
+			continue
+		}
+
+		sop := sV.Field(i).Interface().(*spec.Operation)
+		if sop != nil {
+			ops = append(ops, newOp(method, sop))
+		}
+	}
+	return ops
 }
--- pkg/router/drivers/openapi/spec_loader_test.go
+++ pkg/router/drivers/openapi/spec_loader_test.go
@@ -2,8 +2,10 @@ package openapi
 
 import (
 	"bytes"
+	"encoding/json"
 	"testing"
 
+	"github.com/go-openapi/loads"
 	"github.com/ncatelli/mockserver/pkg/router"
 )
 
@@ -62,5 +64,36 @@ var expectedRoutes = []*router.Route{
 func TestLoadShould(t *testing.T) {
 	t.Run("load a valid configuration", func(t *testing.T) {
 		Load(bytes.NewReader(goodConfig))
+	})
+}
+
+func TestGenerateRouteSwaggerPathShould(t *testing.T) {
+	var knownPath string = "/test/weighted/errors"
+	goodDoc, _ := loads.Analyzed(json.RawMessage(goodConfig), "")
+	goodPath := goodDoc.Spec().Paths.Paths[knownPath]
+	goodOp := &op{Name: "Get", Op: goodPath.Get}
+
+	t.Run("generate a known good route", func(t *testing.T) {
+		r, e := generateRouteFromSwaggerPathOperation(knownPath, goodOp)
+		if e != nil {
+			t.Error(e)
+		}
+
+		if r.Path != knownPath {
+			t.Errorf(errFmt, knownPath, r.Path)
+		}
+	})
+}
+
+func TestGetPathItemOperations(t *testing.T) {
+	var knownPath string = "/test/weighted/errors"
+	goodDoc, _ := loads.Analyzed(json.RawMessage(goodConfig), "")
+	gp := goodDoc.Spec().Paths.Paths[knownPath]
+
+	t.Run("returns expected operations", func(t *testing.T) {
+		ops := getPathItemOperations(gp)
+		if len(ops) != 1 || ops[0].Name != "GET" {
+			t.Errorf(errFmt, "map contained GET", false)
+		}
 	})
 }
